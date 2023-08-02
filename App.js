cfeedb33e0039242035b05b0e860f0218802c138
Chainxc <supawatta.rat_g31@mwit.ac.th>
2023-08-02 21:48:13 +0700

    aasreturn

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {
-  getTheme,
-} from 'react-native-material-kit';
-
-const theme = getTheme();
-
-
 
 export default function App() {
   return (
-    <View style={theme.cardStyle}>
-      <Image source={{uri : base64Icon}} style={theme.cardImageStyle} />
-      <Text style={theme.cardTitleStyle}>Welcome</Text>
-      <Text style={theme.cardContentStyle}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Mauris sagittis pellentesque lacus eleifend lacinia...
-      </Text>
-      <View style={theme.cardMenuStyle}>{menu}</View>
-      <Text style={theme.cardActionStyle}>My Action</Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
